52a6c7b74c32f94c433d4468548494dfcfd873c3
Dave <zr.davaa@gmail.com>
2024-03-08 00:50:13 -0500

    display income expense

--- app.js
+++ app.js
@@ -1,39 +1,59 @@
 // Display controller
-var uiController = (function() {
+var uiController = (function () {
   var DOMstrings = {
     inputType: ".add__type",
     inputDescription: ".add__description",
     inputValue: ".add__value",
-    addBtn: ".add__btn"
+    addBtn: ".add__btn",
   };
 
   return {
-    getInput: function() {
+    getInput: function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // exp, inc
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        value: document.querySelector(DOMstrings.inputValue).value,
       };
     },
 
-    getDOMstrings: function() {
+    getDOMstrings: function () {
       return DOMstrings;
-    }
+    },
+
+    addListItem: function (item, type) {
+      // write and html to contain income, expense
+      var html, list;
+      if (type === "inc") {
+        list = ".income__list";
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
+      } else {
+        list = ".expenses__list";
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+      // on that HTML, use replace function to modify income and expense
+      html = html.replace("%id%", item.id);
+      html = html.replace("$$DESCRIPTION$$", item.description);
+      html = html.replace("$$VALUE$$", item.value);
+      // into DOM
+      document.querySelector(list).insertAdjacentHTML("beforeend", html);
+    },
   };
 })();
 
 // Finance Controller
 // private function
-var financeController = (function() {
+var financeController = (function () {
   // private data
-  var Income = function(id, description, value) {
+  var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
 
   // private data
-  var Expense = function(id, description, value) {
+  var Expense = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
@@ -43,17 +63,17 @@ var financeController = (function() {
   var data = {
     items: {
       inc: [],
-      exp: []
+      exp: [],
     },
 
     totals: {
       inc: 0,
-      exp: 0
-    }
+      exp: 0,
+    },
   };
 
   return {
-    addItem: function(type, desc, val) {
+    addItem: function (type, desc, val) {
       var item, id;
 
       if (data.items[type].length === 0) id = 1;
@@ -68,39 +88,44 @@ var financeController = (function() {
       }
 
       data.items[type].push(item);
+
+      return item;
     },
 
-    seeData: function() {
+    seeData: function () {
       return data;
-    }
+    },
   };
 })();
 
 // App connector controller
-var appController = (function(uiController, financeController) {
-
-  var ctrlAddItem = function() {
+var appController = (function (uiController, financeController) {
+  var ctrlAddItem = function () {
     // 1. Find input data from display
     var input = uiController.getInput();
 
     // 2. Save the data to the finance controller
-    financeController.addItem(input.type, input.description, input.value);
+    var item = financeController.addItem(
+      input.type,
+      input.description,
+      input.value
+    );
 
     // 3. Display the data to right place
-
+    uiController.addListItem(item, input.type);
     // 4. Calculate the budget
 
     // 5. Final remainder, display calculated data
   };
 
-  var setupEventListeners = function() {
+  var setupEventListeners = function () {
     var DOM = uiController.getDOMstrings();
 
-    document.querySelector(DOM.addBtn).addEventListener("click", function() {
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
     });
 
-    document.addEventListener("keypress", function(event) {
+    document.addEventListener("keypress", function (event) {
       if (event.keyCode === 13 || event.which === 13) {
         ctrlAddItem();
       }
@@ -108,10 +133,10 @@ var appController = (function(uiController, financeController) {
   };
 
   return {
-    init: function() {
+    init: function () {
       console.log("Application started...");
       setupEventListeners();
-    }
+    },
   };
 })(uiController, financeController);
 
